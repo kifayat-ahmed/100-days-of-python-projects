c0b583f0edbbeacd448a0c459832efe0f1f52969
kifayat-ahmed <kifayatispro@gmail.com>
2026-01-10 01:42:50 +0600

    day 1 project completed

---
+++ Day_1.py
@@ -0,0 +1,5 @@
+# DAY 1
+print("Welcome to the band name generator!")
+City = input("What is the name of the city you grew up in? ")
+Pet = input("What is the name of your first pet? ")
+print(f"Your band name could be: {City} {Pet}")
